4c181ae55d4940fd6cbcff7fd050bf1b8a7bc1a8
nertsal <nestralnertsal@gmail.com>
2024-04-28 23:52:28 +0300

    Berry and screen splits

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,8 @@ use asr::{
     game_engine::unity::mono::{self, UnityPointer},
     Process,
 };
+use enum_primitive_derive::Primitive;
+use num_traits::FromPrimitive;
 
 asr::async_main!(stable);
 // asr::panic_handler!();
@@ -24,25 +26,54 @@ async fn main() {
                 let player_data_store = GameManagerFinder::wait_attach(&process).await;
 
                 let module = &player_data_store.module;
-                let class = player_data_store
-                    .image
-                    .wait_get_class(&process, module, "GameManager")
-                    .await;
-                asr::print_message(&format!(
-                    "{:?}",
-                    class.get_field_offset(&process, module, "berryCount")
-                ));
 
-                let mut berries = Some(999);
+                // asr::print_message("looking at classes");
+                // for class in player_data_store.image.classes(&process, module) {
+                //     let field = class
+                //         .wait_get_static_instance(&process, module, "Name")
+                //         .await;
+                //     let name: ArrayCString<10> = process.read(field).unwrap_or_default();
+                //     asr::print_message(name.validate_utf8().unwrap_or_default());
+                // }
+
+                // let class = player_data_store
+                //     .image
+                //     .wait_get_class(&process, module, "Player")
+                //     .await;
+                // asr::print_message(&format!(
+                //     "{:?}",
+                //     class.get_field_offset(&process, module, "state")
+                // ));
+
+                let mut berries = -1;
+                let mut player_state = PlayerState::Idk;
 
                 asr::print_message("Entering loop");
                 loop {
-                    let new_berries = player_data_store.get_berry_count(&process);
-
+                    let new_berries = player_data_store.get_berry_count(&process).unwrap_or(0);
                     if berries != new_berries {
-                        asr::print_message(&format!("player has {:?} berries", new_berries));
+                        asr::print_message(&format!("player now has {:?} berries", new_berries));
+                        if new_berries > berries {
+                            // Berry collected
+                            asr::print_message("berry split");
+                            // asr::timer::split();
+                        } else if new_berries == 0 {
+                            asr::print_message("berry reset");
+                            // asr::timer::reset();
+                        }
                     }
                     berries = new_berries;
+
+                    let new_player_state = player_data_store
+                        .get_player_state(&process)
+                        .unwrap_or(PlayerState::Idk);
+                    if player_state != new_player_state {
+                        asr::print_message(&format!("player state is now {:?}", new_player_state));
+                        if let PlayerState::AutoMoving = new_player_state {
+                            asr::print_message("screen transition");
+                        }
+                    }
+                    player_state = new_player_state;
 
                     next_tick().await;
                 }
@@ -108,16 +139,34 @@ impl GameManagerFinder {
             .deref(process, &self.module, &self.image)
             .ok()
     }
+
+    pub fn get_player_state(&self, process: &Process) -> Option<PlayerState> {
+        let state: i32 = self
+            .player_data_pointers
+            .player_state
+            .deref(process, &self.module, &self.image)
+            .ok()?;
+        PlayerState::from_i32(state)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Primitive)]
+enum PlayerState {
+    Normal = 0,
+    Idk = 1,
+    AutoMoving = 2,
 }
 
 struct PlayerDataPointers {
-    berries: UnityPointer<3>,
+    berries: UnityPointer<2>,
+    player_state: UnityPointer<3>,
 }
 
 impl PlayerDataPointers {
     pub fn new() -> Self {
         Self {
             berries: UnityPointer::new("GameManager", 0, &["instance", "berryCount"]),
+            player_state: UnityPointer::new("Player", 0, &["instance", "state"]),
         }
     }
 }
