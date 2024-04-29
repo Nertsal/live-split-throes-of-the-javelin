cdf3b43be61d681204a99bbef10e708abe1b3efa
nertsal <nestralnertsal@gmail.com>
2024-04-29 18:51:37 +0300

    Splits

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ use alloc::format;
 use asr::{
     future::next_tick,
     game_engine::unity::mono::{self, UnityPointer},
+    watcher::Watcher,
     Process,
 };
 use enum_primitive_derive::Primitive;
@@ -14,72 +15,125 @@ asr::async_main!(stable);
 // asr::panic_handler!();
 
 async fn main() {
-    // TODO: Set up some general state and settings.
+    // Set up some general state and settings.
+    let splits = vec![Split::Start, Split::Key];
 
     asr::print_message("Hello, World!");
 
     loop {
+        asr::print_message("Trying to attach to the game...");
         let process = Process::wait_attach("Throes of the Javelin.exe").await;
         process
             .until_closes(async {
+                asr::print_message("Attached to the process, reading memory...");
+
                 // Load some initial information from the process.
-                let player_data_store = GameManagerFinder::wait_attach(&process).await;
 
-                let module = &player_data_store.module;
+                let game = GameManagerFinder::wait_attach(&process).await;
 
-                // asr::print_message("looking at classes");
-                // for class in player_data_store.image.classes(&process, module) {
-                //     let field = class
-                //         .wait_get_static_instance(&process, module, "Name")
-                //         .await;
-                //     let name: ArrayCString<10> = process.read(field).unwrap_or_default();
-                //     asr::print_message(name.validate_utf8().unwrap_or_default());
-                // }
+                let mut controller = Controller::new(splits.clone());
 
-                // let class = player_data_store
-                //     .image
-                //     .wait_get_class(&process, module, "Player")
-                //     .await;
-                // asr::print_message(&format!(
-                //     "{:?}",
-                //     class.get_field_offset(&process, module, "state")
-                // ));
-
-                let mut berries = -1;
-                let mut player_state = PlayerState::Idk;
+                let mut key_collected = Watcher::<bool>::new();
+                let mut berries = Watcher::<i32>::new();
+                let mut transitions = Watcher::<i32>::new();
+                let mut player_state = Watcher::<PlayerState>::new();
+                let mut started = Watcher::<bool>::new();
+                let mut finished = Watcher::<bool>::new();
 
                 asr::print_message("Entering loop");
                 loop {
-                    let new_berries = player_data_store.get_berry_count(&process).unwrap_or(0);
-                    if berries != new_berries {
-                        asr::print_message(&format!("player now has {:?} berries", new_berries));
-                        if new_berries > berries {
-                            // Berry collected
-                            asr::print_message("berry split");
-                            // asr::timer::split();
-                        } else if new_berries == 0 {
-                            asr::print_message("berry reset");
-                            // asr::timer::reset();
+                    // asr::print_message(&format!("{:?}", game.started(&process)));
+                    if let Some(pair) = started.update(game.started(&process)) {
+                        if pair.changed_to(&true) {
+                            controller.split(Split::Start);
+                        } else if pair.changed_to(&false) {
+                            controller.reset();
                         }
                     }
-                    berries = new_berries;
+                    if let Some(pair) = finished.update(game.finished(&process)) {
+                        if pair.changed_to(&true) {
+                            controller.split(Split::BigBerry);
+                        }
+                    }
 
-                    let new_player_state = player_data_store
-                        .get_player_state(&process)
-                        .unwrap_or(PlayerState::Idk);
-                    if player_state != new_player_state {
-                        asr::print_message(&format!("player state is now {:?}", new_player_state));
-                        if let PlayerState::AutoMoving = new_player_state {
-                            asr::print_message("screen transition");
+                    if let Some(pair) = key_collected.update(game.get_key_collected(&process)) {
+                        if pair.changed_to(&true) {
+                            controller.split(Split::Key);
                         }
                     }
-                    player_state = new_player_state;
+
+                    if let Some(pair) = berries.update(game.get_berry_count(&process)) {
+                        if pair.increased() {
+                            controller.split(Split::Berry(pair.current));
+                        }
+                    }
+
+                    if let Some(pair) = transitions.update(game.get_transition_count(&process)) {
+                        if pair.increased() {
+                            controller.split(Split::ScreenTransition);
+                        }
+                    }
+
+                    if let Some(pair) = player_state.update(game.get_player_state(&process)) {
+                        if pair.changed() {
+                            if let PlayerState::AutoMoving = pair.current {
+                                controller.split(Split::ScreenTransition);
+                            }
+                        }
+                    }
 
                     next_tick().await;
                 }
             })
             .await;
     }
+}
+
+struct Controller {
+    splits: Vec<Split>,
+    next_split: usize,
+}
+
+impl Controller {
+    pub fn new(splits: Vec<Split>) -> Self {
+        Self {
+            splits,
+            next_split: 0,
+        }
+    }
+
+    pub fn split(&mut self, split: Split) {
+        asr::print_message(&format!("split {:?}", split));
+        if let Some(&current) = self.splits.get(self.next_split) {
+            if current == split {
+                if let Split::Start = split {
+                    asr::timer::start();
+                } else {
+                    asr::timer::split();
+                }
+            }
+        }
+    }
+
+    pub fn reset(&mut self) {
+        asr::timer::reset();
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Split {
+    Start,
+    Key,
+    Berry(i32),
+    ScreenTransition,
+    BigBerry,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Primitive)]
+enum PlayerState {
+    Alive = 0,
+    Dying = 1,
+    AutoMoving = 2,
 }
 
 struct GameManagerFinder {
@@ -105,7 +159,7 @@ impl GameManagerFinder {
         //     "GameManagerFinder wait_attach: pointer_size = {:?}",
         //     pointer_size
         // ));
-        asr::print_message("GameManagerFinder wait_attach: Module wait_attach_auto_detect...");
+        // asr::print_message("GameManagerFinder wait_attach: Module wait_attach_auto_detect...");
         next_tick().await;
         let mut found_module = false;
         let mut needed_retry = false;
@@ -113,12 +167,12 @@ impl GameManagerFinder {
             let module = mono::Module::wait_attach_auto_detect(process).await;
             if !found_module {
                 found_module = true;
-                asr::print_message("GameManagerFinder wait_attach: module get_default_image...");
+                // asr::print_message("GameManagerFinder wait_attach: module get_default_image...");
                 next_tick().await;
             }
             for _ in 0..0x10 {
                 if let Some(image) = module.get_default_image(process) {
-                    asr::print_message("GameManagerFinder wait_attach: got module and image");
+                    // asr::print_message("GameManagerFinder wait_attach: got module and image");
                     next_tick().await;
                     // return GameManagerFinder::new(pointer_size, module, image);
                     return GameManagerFinder::new(module, image);
@@ -127,7 +181,7 @@ impl GameManagerFinder {
             }
             if !needed_retry {
                 needed_retry = true;
-                asr::print_message("GameManagerFinder wait_attach: retry...");
+                // asr::print_message("GameManagerFinder wait_attach: retry...");
                 next_tick().await;
             }
         }
@@ -148,24 +202,53 @@ impl GameManagerFinder {
             .ok()?;
         PlayerState::from_i32(state)
     }
-}
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Primitive)]
-enum PlayerState {
-    Normal = 0,
-    Idk = 1,
-    AutoMoving = 2,
+    pub fn get_key_collected(&self, process: &Process) -> Option<bool> {
+        self.player_data_pointers
+            .key_collected
+            .deref(process, &self.module, &self.image)
+            .ok()
+    }
+
+    pub fn get_transition_count(&self, process: &Process) -> Option<i32> {
+        self.player_data_pointers
+            .transition_count
+            .deref(process, &self.module, &self.image)
+            .ok()
+    }
+
+    pub fn started(&self, process: &Process) -> Option<bool> {
+        self.player_data_pointers
+            .started
+            .deref(process, &self.module, &self.image)
+            .ok()
+    }
+
+    pub fn finished(&self, process: &Process) -> Option<bool> {
+        self.player_data_pointers
+            .finished
+            .deref(process, &self.module, &self.image)
+            .ok()
+    }
 }
 
 struct PlayerDataPointers {
     berries: UnityPointer<2>,
-    player_state: UnityPointer<3>,
+    key_collected: UnityPointer<2>,
+    transition_count: UnityPointer<2>,
+    started: UnityPointer<2>,
+    finished: UnityPointer<2>,
+    player_state: UnityPointer<2>,
 }
 
 impl PlayerDataPointers {
     pub fn new() -> Self {
         Self {
             berries: UnityPointer::new("GameManager", 0, &["instance", "berryCount"]),
+            key_collected: UnityPointer::new("GameManager", 0, &["instance", "hasKeyCollected"]),
+            transition_count: UnityPointer::new("GameManager", 0, &["instance", "transitionCount"]),
+            started: UnityPointer::new("UIManager", 0, &["instance", "speedrunStarted"]),
+            finished: UnityPointer::new("UIManager", 0, &["instance", "endedSpeedrun"]),
             player_state: UnityPointer::new("Player", 0, &["instance", "state"]),
         }
     }
